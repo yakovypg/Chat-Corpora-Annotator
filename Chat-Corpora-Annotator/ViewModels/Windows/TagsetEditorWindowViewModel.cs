db40eefa560055ed61c9e19c0e319f8a732fa36c
yakovypg <yasha-pmp@mail.ru>
2021-10-27 19:40:49 +0300

    Changed the appearance of the tagset editor window

--- Chat-Corpora-Annotator/ViewModels/Windows/TagsetEditorWindowViewModel.cs
+++ Chat-Corpora-Annotator/ViewModels/Windows/TagsetEditorWindowViewModel.cs
@@ -5,12 +5,38 @@ using System.Linq;
 using System.Windows.Input;
 using System.Threading.Tasks;
 using ChatCorporaAnnotator.Infrastructure.Commands;
+using System.Collections.ObjectModel;
+using ChatCorporaAnnotator.Models.Chat;
 
 namespace ChatCorporaAnnotator.ViewModels.Windows
 {
     internal class TagsetEditorWindowViewModel : ViewModel
     {
         public Action DeactivateAction { get; set; }
+
+        public ObservableCollection<string> Tagsets { get; private set; }
+        public ObservableCollection<Tag> CurrentTagset { get; private set; }
+
+        private Tag _selectedTag;
+        public Tag SelectedTag
+        {
+            get => _selectedTag;
+            set => SetValue(ref _selectedTag, value);
+        }
+
+        private string _selectedTagset;
+        public string SelectedTagset
+        {
+            get => _selectedTagset;
+            set => SetValue(ref _selectedTagset, value);
+        }
+
+        private string _consoleText;
+        public string ConsoleText
+        {
+            get => _consoleText;
+            set => SetValue(ref _consoleText, value);
+        }
 
         #region SystemCommands
 
